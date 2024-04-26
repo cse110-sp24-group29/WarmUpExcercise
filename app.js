3b568594b473f7d1cd4eca9425ebf8e2b919a8ad
sakshamrai101 <77190532+sakshamrai101@users.noreply.github.com>
2024-04-26 14:21:43 -0700

    Sentiment-Widget and Some Styling Added

--- app.js
+++ app.js
@@ -4,6 +4,58 @@ document.addEventListener("DOMContentLoaded", function () {
 
     submitButton.addEventListener("click", function (event) {
         event.preventDefault();
-        window.location.href = "ty-page.html";
+        window.location.href = 'ty-page.html';
     });
 });
+
+document
+    .getElementById("hamburger-menu")
+    .addEventListener("click", function () {
+        const navLinks = document.getElementById("nav-links");
+        navLinks.classList.toggle("active");
+    });
+
+document.querySelectorAll('.star').forEach(star => {
+    star.addEventListener('mouseover', function () {
+        resetStars();
+        this.classList.add('hover-active');
+        const index = this.getAttribute('data-value');
+        highlightStars(index);
+        document.getElementById('selectedCaption').textContent = this.getAttribute('data-caption');
+        document.getElementById('selectedEmoji').textContent = this.getAttribute('data-emoji');
+    });
+
+    star.addEventListener('mouseout', function () {
+        resetStars();
+    });
+
+    star.addEventListener('click', function () {
+        resetStars();
+        this.classList.add('active');
+        const index = this.getAttribute('data-value');
+        highlightStars(index, true);
+        document.getElementById('selectedCaption').textContent = this.getAttribute('data-caption');
+        document.getElementById('selectedEmoji').textContent = this.getAttribute('data-emoji');
+    });
+});
+
+function highlightStars(index, keep = false) {
+    document.querySelectorAll('.star').forEach(star => {
+        if (star.getAttribute('data-value') <= index) {
+            if (keep) {
+                star.classList.add('active');
+            } else {
+                star.classList.add('hover-active');
+            }
+        }
+    });
+}
+
+function resetStars() {
+    document.querySelectorAll('.star').forEach(star => {
+        star.classList.remove('hover-active', 'active');
+    });
+}
+
+
+
